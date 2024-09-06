2f31e3ce558e768d2b10b1c721f42d7db62e3adc
brittf619 <brittfrenkel19@gmail.com>
2024-09-07 00:15:58 +0300

    added default vendor func

--- src/vendors.ts
+++ src/vendors.ts
@@ -1,4 +1,5 @@
 import config from "./config.json"
+import logger from "./logger"
 
 interface VendorConfig {
   rateLimit: number;
@@ -13,10 +14,31 @@ interface Config {
 
 const typedConfig: Config = config;
 
+function isValidVendor(vendor: string): boolean {
+    return vendor in typedConfig.vendors;
+};
+  
+function getDefaultVendor(): string{
+    let defaultVendor: string = typedConfig.defaultVendor;
+    if (!isValidVendor(defaultVendor)) {
+        /* TODO - check this in a test */
+        logger.error(`Invalid default vendor: ${defaultVendor}. Using fallback.`);
+        defaultVendor = Object.keys(typedConfig.vendors)[0];
+    }
 
-function getCountryName(ipADdress: string){
-    const defaultVendor: string = typedConfig.defaultVendor;
+    return defaultVendor;
+};
+
+function getCountryName(ipADdress: string): string{
+    const defaultVendor: string = getDefaultVendor();
     const limit = typedConfig.vendors[defaultVendor].rateLimit;
+
+    if (defaultVendor == "ipstack") {
+        const s = "";
+    } else if (defaultVendor == "callme") {
+        const d = "";
+    } else {
+    }
     return "";
 }
 
